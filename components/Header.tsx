aed7ab2be0f4f310b857d2cc89e94be3890aeef3
Michele Carmagnani <michele.carmagnani@gmail.com>
2022-01-20 15:04:19 +0100

    working on fetching from sanity

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,25 +3,28 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex justify-between p-5 max-w-7xl max-auto">
+    <header className="flex justify-between p-5 max-w-7xl ">
+      <div className="flex items-center  space-x-5">
         <Link href="/post">
           <img
-            className="w-44 object cursor-pointer"
+            className="w-44 object-contain cursor-pointer"
             src="https://links.papareact.com/yvf"
             alt=""
           />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-            <h3>About</h3>
-            <h3>Contanct</h3>
-            <h3 className='text-white bg-green-600'>Follow</h3>
+          <h3>About</h3>
+          <h3>Contanct</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
         </div>
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
-        <h3 className='border px-4 py-1 rounded-full border-green-600'>Get Started</h3>
-        
       </div>
+      <div className="flex items-center space-x-5 text-green-600 ">
+        <h3 className="cursor-pointer hover:text-black">Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600 cursor-pointer hover:bg-green-600 hover:text-white ">
+          Get Started
+        </h3>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,49 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import { sanityClient } from '../sanity';
+import { sanityClient, urlFor } from '../sanity';
 import { Post } from '../Typings';
 interface Props {
   posts: [Post];
 }
 
 export default function Home({ posts }: Props) {
-  console.log(process.env.NEXT_PUBLIC_SANITY_DATASET );
-  
+  console.log(posts)
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="flex justify-between items-center bg-yellow-400 border-y-2 border-black py-10 lg:py-0">
-        <div className="px-10 space-y-5">
-          <div>
-            <h1 className="text-6xl max-w-xl font-serif">
-              <span className="underline decoration-black decoration-4">
-                Medium{' '}
-              </span>
-              is place to write, read and connect{' '}
-            </h1>
-          </div>
-          <h2>it's easy and free to post your thinking </h2>
+      <div className="flex justify-between items-center bg-yellow-400 border-y-2 border-black py-10 lg:py-0 ">
+        <div className=" px-10 space-y-5">
+          <h1 className=" md:text-4xl text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{' '}
+            is place to write, read and connect{' '}
+          </h1>
+          <h2>
+            it's easy and free to post your thinking on any topicc and connect
+            with milllions of readers.
+          </h2>
         </div>
-        <div>
-          <img className="hidden md:inline-flex h-32 lg:h-full" src="" alt="" />
-        </div>
+
+        <img
+          className="hidden md:inline-flex h-30 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt="Medium"
+        />
       </div>
     </div>
   );
 }
 
 export const getServerSideProps = async () => {
-  const query = `*[_type == "post"]{
+  const query = `
+  *[_type == "post"]{
     _id,
     title,
-    slug,
     author -> {
     name,
     image
@@ -49,9 +52,9 @@ export const getServerSideProps = async () => {
   mainImage,
   slug
   }`;
-
-  const post = await sanityClient.fetch(query);
+  
+  const posts = await sanityClient.fetch(query);
   return {
-    props: { post },
+    posts,
   };
 };
